beff555aec71343cac8edce78079d94094a7fc06
Luis Jaramillo <dev.luisjn@gmail.com>
2025-08-08 05:13:55 -0500

    Setup inventory player controller

---
+++ Plugins/Inventory/Source/Inventory/Private/Player/Inv_PlayerController.cpp
@@ -0,0 +1,37 @@
+﻿// 
+
+
+#include "Player/Inv_PlayerController.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+void AInv_PlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+}
+
+void AInv_PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	if (IsValid(Subsystem))
+	{
+		for (UInputMappingContext* CurrentContext : DefaultIMCs)
+		{
+			Subsystem->AddMappingContext(CurrentContext, 0);
+		}
+	}
+
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	EnhancedInputComponent->BindAction(PrimaryInteractAction, ETriggerEvent::Started, this, &AInv_PlayerController::PrimaryIntectact);
+}
+
+void AInv_PlayerController::PrimaryIntectact()
+{
+	UE_LOG(LogTemp, Log, TEXT("Primary Interact"));
+}
---
+++ Plugins/Inventory/Source/Inventory/Public/Player/Inv_PlayerController.h
@@ -0,0 +1,28 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Inv_PlayerController.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+
+UCLASS()
+class INVENTORY_API AInv_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+
+private:
+	void PrimaryIntectact();
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
+	TArray<TObjectPtr<UInputMappingContext>> DefaultIMCs;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
+	TObjectPtr<UInputAction> PrimaryInteractAction;
+};
